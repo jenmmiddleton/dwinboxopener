b14d7da781ecf15dbaa13ae71dbbf890d58a9237
frudence <jenmmiddleton@gmail.com>
2021-03-28 14:07:52 -0400

    Trying to get a single link clicked, but not working.

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
 chrome.action.onClicked.addListener(async () => {
   let url = "https://www.dreamwidth.org/inbox/";
   let tab = await chrome.tabs.create({ url });
-  console.log(`Created tab ${tab.id}`);
+  console.log("Created tab ${tab.id}");
+  sleep(2000);
+  document.getElementsByClassName("InboxItem_Content usercontent").getElementsByClassName("actions")[0].getElementsByTagName("a")[2].click();
+  console.log("Clicked link");
 });
