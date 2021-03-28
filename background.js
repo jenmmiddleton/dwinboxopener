ec7a3f4d3c880f9991f8bfacbb12a24dfa1b9c78
frudence <jenmmiddleton@gmail.com>
2021-03-28 12:54:56 -0400

    Back to basics 2

--- background.js
+++ background.js
@@ -3,17 +3,3 @@ chrome.runtime.onInstalled.addListener(async () => {
   let tab = await chrome.tabs.create({ url });
   console.log(`Created tab ${tab.id}`);
 });
-
-//function reddenPage() {
-//  document.body.style.backgroundColor = 'red';
-//  url = "https://www.dreamwidth.org/inbox/";
-//  let tab = await chrome.tabs.create(url);
-//  console.log(`Created tab ${tab.id}`);
-//  document.getElementById('$0').click();
-//}
-//
-//chrome.action.onClicked.addListener((tab) => {
-//  chrome.scripting.executeScript({
-//    function: reddenPage
-//  });
-//});
