767eb968ffded45f79622ac1dad774b8f97655ea
frudence <jenmmiddleton@gmail.com>
2021-03-28 14:22:48 -0400

    Still erroring at line 5

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 chrome.action.onClicked.addListener(async () => {
   let url = "https://www.dreamwidth.org/inbox/";
   let tab = await chrome.tabs.create({ url });
-  console.log("Created tab ${tab.id}");
-  sleep(2000);
-  document.getElementsByClassName("InboxItem_Content usercontent").getElementsByClassName("actions")[0].getElementsByTagName("a")[2].click();
-  console.log("Clicked link");
+  console.log(`Created tab ${tab.id}`);
+  let parentLink = await document.getElementsByClassName("InboxItem_Content usercontent").getElementsByClassName("actions")[0].getElementsByTagName("a")[2];
+  parentLink.click();
+  console.log(`Clicked link`);
 });
