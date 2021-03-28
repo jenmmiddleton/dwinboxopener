bfb1a407189f8e44b9f50275fdf7735a0447a376
frudence <jenmmiddleton@gmail.com>
2021-03-28 12:41:58 -0400

    Added tabs permissions

--- background.js
+++ background.js
@@ -1,5 +1,16 @@
-function openParentLink() {
-  let url = chrome.runtime.getURL("https://www.dreamwidth.org/inbox/");
+//function openParentLink() {
+//}
+//
+//chrome.action.onClicked.addListener((tab) => {
+//  chrome.scripting.executeScript({
+//    target: { tabId: tab.id },
+//    function: openParentLink
+//  });
+//});
+
+function reddenPage() {
+  document.body.style.backgroundColor = 'red';
+  let url = "https://www.dreamwidth.org/inbox/";
   let tab = await chrome.tabs.create({ url });
   console.log(`Created tab ${tab.id}`);
   document.getElementById('$0').click();
@@ -8,6 +19,6 @@ function openParentLink() {
 chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: openParentLink
+    function: reddenPage
   });
 });
