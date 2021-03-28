a1e5e7f18b07e44704cd0a979738613233b5f3fd
frudence <jenmmiddleton@gmail.com>
2021-03-28 12:51:47 -0400

    Back to basics

--- background.js
+++ background.js
@@ -1,24 +1,19 @@
-//function openParentLink() {
+chrome.runtime.onInstalled.addListener(async () => {
+  let url = chrome.runtime.getURL("https://www.dreamwidth.org/inbox/");
+  let tab = await chrome.tabs.create({ url });
+  console.log(`Created tab ${tab.id}`);
+});
+
+//function reddenPage() {
+//  document.body.style.backgroundColor = 'red';
+//  url = "https://www.dreamwidth.org/inbox/";
+//  let tab = await chrome.tabs.create(url);
+//  console.log(`Created tab ${tab.id}`);
+//  document.getElementById('$0').click();
 //}
 //
 //chrome.action.onClicked.addListener((tab) => {
 //  chrome.scripting.executeScript({
-//    target: { tabId: tab.id },
-//    function: openParentLink
+//    function: reddenPage
 //  });
 //});
-
-function reddenPage() {
-  document.body.style.backgroundColor = 'red';
-  let url = "https://www.dreamwidth.org/inbox/";
-  let tab = await chrome.tabs.create({ url });
-  console.log(`Created tab ${tab.id}`);
-  document.getElementById('$0').click();
-}
-
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: reddenPage
-  });
-});
