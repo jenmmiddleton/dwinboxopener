df51c8eca1a273c0fd427ff34e1f7f13e9b2d1de
frudence <jenmmiddleton@gmail.com>
2021-03-29 21:53:34 -0400

    For loop works better than a while loop cuz I accidentally made an infinite loop.

--- openParentLink.js
+++ openParentLink.js
@@ -1,39 +1,41 @@
 var main_inbox_url = "https://www.dreamwidth.org/inbox/";
 var inbox_unread_url = window.location.href;
+var max_index = 10;
 
-if not(document.getElementsById('NoMessageTD') {
-
-    Number index = 0;
-    do {
+if (document.getElementById('NoMessageTD')) {
+  console.log(`No unread tags`);
+  window.open(main_inbox_url, '_self');
+} else {
+    var index = 0;
+    for ( ; index <= max_index; index++) {
         try {
-          document.getElementsByClassName('InboxItem_Content usercontent')[{index}].getElementsByClassName('actions')[0].getElementsByTagName("a")[2].click();
-          console.log(`Clicked a Parent link`);
-          window.open(inbox_unread_url, '_blank');
+          document.getElementsByClassName('InboxItem_Content usercontent')[index].getElementsByClassName('actions')[0].getElementsByTagName("a")[2].click();
+          console.log(`Clicked parent link`);
+          setTimeout(() => {  window.open(inbox_unread_url, '_blank'); }, 1800);
+          break;
         } catch ( errParentlink ) {
           if (errParentlink instanceof TypeError) {
             try {
-              document.getElementsByClassName('InboxItem_Content usercontent')[{index}].getElementsByClassName('InboxItem_Unread')[0].getElementsByTagName("a")[1].click();
+              document.getElementsByClassName('InboxItem_Content usercontent')[index].getElementsByClassName('actions')[0].getElementsByTagName("a")[1].click();
               console.log(`Clicked an Entry or top-level link`);
-              window.open(inbox_unread_url, '_blank');
+              setTimeout(() => {  window.open(inbox_unread_url, '_blank'); }, 1800);
+              break;
             } catch ( errEntryLink) {
-              if (errParentlink instanceof TypeError) {
-                index = index + 1;
+              if (errEntryLink instanceof TypeError) {
                 console.log(`This is either a Message, a Circle Update, a Site Notice, or an Unauthorized comment.`)
               }
             }
           }
         }
-    } while (index <= 10);
-
-    console.log(`No more unread tags on current page`);
-    var next_button = document.getElementsById('Page_Next_1');
-    if (next_button.disabled) {
-        console.log(`This is the last page of the inbox`);
-        window.open(main_inbox_url, '_self');
-    } else {
-        next_button.click();
+        if (index > max_index) {
+          console.log(`No more unread tags on current page`);
+          var next_button = document.getElementsById('Page_Next_1');
+          if (next_button.disabled) {
+              console.log(`This is the last page of the inbox`);
+              window.open(main_inbox_url, '_self');
+          } else {
+              next_button.click();
+          }
+        }
     }
-} else {
-    console.log(`No unread tags`);
-    window.open(main_inbox_url, '_self');
 }
