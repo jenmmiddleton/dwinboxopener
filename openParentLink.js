42f4bf0af9bf0a57cc66503f686c767f499d8933
frudence <jenmmiddleton@gmail.com>
2021-03-29 22:10:37 -0400

    Includes Delete notifications. Moved Next button block to the correct spot outside of the For loop.

--- openParentLink.js
+++ openParentLink.js
@@ -11,31 +11,31 @@ if (document.getElementById('NoMessageTD')) {
         try {
           document.getElementsByClassName('InboxItem_Content usercontent')[index].getElementsByClassName('actions')[0].getElementsByTagName("a")[2].click();
           console.log(`Clicked parent link`);
-          setTimeout(() => {  window.open(inbox_unread_url, '_blank'); }, 1800);
+          setTimeout(() => {  window.open(inbox_unread_url, '_blank'); }, 900);
           break;
         } catch ( errParentlink ) {
           if (errParentlink instanceof TypeError) {
             try {
               document.getElementsByClassName('InboxItem_Content usercontent')[index].getElementsByClassName('actions')[0].getElementsByTagName("a")[1].click();
               console.log(`Clicked an Entry or top-level link`);
-              setTimeout(() => {  window.open(inbox_unread_url, '_blank'); }, 1800);
+              setTimeout(() => {  window.open(inbox_unread_url, '_blank'); }, 900);
               break;
             } catch ( errEntryLink) {
               if (errEntryLink instanceof TypeError) {
-                console.log(`This is either a Message, a Circle Update, a Site Notice, or an Unauthorized comment.`)
+                console.log(`This is either a Message, a Circle Update, a Site Notice, Delete notifications, or an Unauthorized comment.`)
               }
             }
           }
         }
-        if (index > max_index) {
-          console.log(`No more unread tags on current page`);
-          var next_button = document.getElementById('Page_Next_1');
-          if (next_button.disabled) {
-              console.log(`This is the last page of the inbox`);
-              window.open(main_inbox_url, '_self');
-          } else {
-              next_button.click();
-          }
-        }
+    }
+    if (index > max_index) {
+      console.log(`No more unread tags on current page`);
+      var next_button = document.getElementById('Page_Next_1');
+      if (next_button.disabled) {
+          console.log(`This is the last page of the inbox`);
+          window.open(main_inbox_url, '_self');
+      } else {
+          next_button.click();
+      }
     }
 }
