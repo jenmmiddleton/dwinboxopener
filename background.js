43e0953f654623b87adaf2b1b50a9c0ec9bbed89
frudence <jenmmiddleton@gmail.com>
2021-03-29 21:10:47 -0400

    Trying to skip Messages, Circle Updates, Site Notices, and Unauthorized comments. This should leave comment responses, new entry notices, and top-level responses to entries.

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.action.onClicked.addListener(async () => {
-  var url = "https://www.dreamwidth.org/inbox/?view=unread";
+  var url = "https://www.dreamwidth.org/inbox/?page=1&view=unread";
   let tab = await chrome.tabs.create({ url });
   console.log(`Created tab ${tab.id}`);
 });
--- openParentLink.js
+++ openParentLink.js
@@ -1,12 +1,39 @@
-var inbox_unread_url = "https://www.dreamwidth.org/inbox/?view=unread";
+var main_inbox_url = "https://www.dreamwidth.org/inbox/";
+var inbox_unread_url = window.location.href;
 
-try {
-  document.getElementsByClassName('InboxItem_Content usercontent')[0].getElementsByClassName('actions')[0].getElementsByTagName("a")[2].click();
-  console.log(`Clicked link`);
-  window.open(inbox_unread_url, '_blank');
-} catch ( err ) {
-  if (err instanceof TypeError) {
+if not(document.getElementsById('NoMessageTD') {
+
+    Number index = 0;
+    do {
+        try {
+          document.getElementsByClassName('InboxItem_Content usercontent')[{index}].getElementsByClassName('actions')[0].getElementsByTagName("a")[2].click();
+          console.log(`Clicked a Parent link`);
+          window.open(inbox_unread_url, '_blank');
+        } catch ( errParentlink ) {
+          if (errParentlink instanceof TypeError) {
+            try {
+              document.getElementsByClassName('InboxItem_Content usercontent')[{index}].getElementsByClassName('InboxItem_Unread')[0].getElementsByTagName("a")[1].click();
+              console.log(`Clicked an Entry or top-level link`);
+              window.open(inbox_unread_url, '_blank');
+            } catch ( errEntryLink) {
+              if (errParentlink instanceof TypeError) {
+                index = index + 1;
+                console.log(`This is either a Message, a Circle Update, a Site Notice, or an Unauthorized comment.`)
+              }
+            }
+          }
+        }
+    } while (index <= 10);
+
+    console.log(`No more unread tags on current page`);
+    var next_button = document.getElementsById('Page_Next_1');
+    if (next_button.disabled) {
+        console.log(`This is the last page of the inbox`);
+        window.open(main_inbox_url, '_self');
+    } else {
+        next_button.click();
+    }
+} else {
     console.log(`No unread tags`);
-    window.close();
-  }
+    window.open(main_inbox_url, '_self');
 }
