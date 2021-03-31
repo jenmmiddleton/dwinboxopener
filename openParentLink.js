dd923f013a0e23bd2542ff04f647f40a49ef9dca
frudence <jenmmiddleton@gmail.com>
2021-03-30 22:47:01 -0400

    Fixing the logic around how to check for uninteractable elements after I took away the nested try/catch blocks.

--- openParentLink.js
+++ openParentLink.js
@@ -1,5 +1,6 @@
 var main_inbox_url = "https://www.dreamwidth.org/inbox/";
 var inbox_unread_url = window.location.href;
+var next_button = document.getElementById('Page_Next_1');
 
 var max_tags_key = "max_tags";
 var max_tags_value = localStorage.getItem(max_tags_key);
@@ -16,47 +17,62 @@ if (document.getElementById('NoMessageTD')) {
   localStorage.removeItem(max_tags_key);
 } else {
     var user_elements = document.getElementsByClassName('InboxItem_Content usercontent');
-    var index = 0;
-    var max_index = Math.min(max_tags_value, user_elements.length);
-    for ( ; index <= max_index; index++) {
-        var actions_links = user_elements[index].getElementsByClassName('actions')[0].getElementsByTagName("a");
-
-        if (actions_links === null) {
-          console.log(`This is either a Message, a Circle Update, a Site Notice, Delete notifications, or an Unauthorized comment.`);
-        } else if (actions_links.length === 2) {
-
-          actions_links[1].click();
-          setTimeout(() => { console.log(`Clicked an Entry or top-level link`); }, 900);
-          localStorage.removeItem(max_tags_key);
-          var new_max_tags = parseInt(max_tags_value) - 1;
-          localStorage.setItem(max_tags_key, new_max_tags);
-          console.log(`New Max Tags: ${new_max_tags}`);
-          window.open(inbox_unread_url, '_blank');
-          break;
-
+    console.log(`Number of interactable elements: ${user_elements.length}`);
+    if (user_elements.length === 0) {
+        if (next_button.disabled) {
+            console.log(`This is the last page of the inbox`);
+            window.open(main_inbox_url, '_self');
         } else {
+            next_button.click();
+        }
+    } else {
+        var index = 0;
+        var max_index = Math.min(max_tags_value, user_elements.length);
+        for ( ; index <= max_index; index++) {
+            var actions_area = user_elements[index].getElementsByClassName('actions');
+            var actions_links;
+            try {
+                actions_links = actions_area[0].getElementsByTagName("a");
+            } catch ( err ) {
+                console.log(`There are no links in the action area to click.`)
+            }
 
-          actions_links[2].click();
-          setTimeout(() => { console.log(`Clicked parent link`); }, 900);
-          localStorage.removeItem(max_tags_key);
-          var new_max_tags = parseInt(max_tags_value) - 1;
-          localStorage.setItem(max_tags_key, new_max_tags);
-          console.log(`New Max Tags: ${new_max_tags}`);
-          window.open(inbox_unread_url, '_blank');
-          break;
+            if (actions_area === null || actions_links === null) {
+              console.log(`This is either a Message, a Circle Update, a Site Notice, Delete notifications, or an Unauthorized comment.`);
+            } else if (actions_links.length == 2) {
+
+              actions_links[1].click();
+              setTimeout(() => { console.log(`Clicked an Entry or top-level link`); }, 900);
+              localStorage.removeItem(max_tags_key);
+              var new_max_tags = parseInt(max_tags_value) - 1;
+              localStorage.setItem(max_tags_key, new_max_tags);
+              console.log(`New Max Tags: ${new_max_tags}`);
+              window.open(inbox_unread_url, '_blank');
+              break;
+
+            } else {
+
+              actions_links[2].click();
+              setTimeout(() => { console.log(`Clicked parent link`); }, 900);
+              localStorage.removeItem(max_tags_key);
+              var new_max_tags = parseInt(max_tags_value) - 1;
+              localStorage.setItem(max_tags_key, new_max_tags);
+              console.log(`New Max Tags: ${new_max_tags}`);
+              window.open(inbox_unread_url, '_blank');
+              break;
+
+            }
+        }
+        if (index > max_index) {
+
+          console.log(`No more unread tags on current page`);
+          if (next_button.disabled) {
+              console.log(`This is the last page of the inbox`);
+              window.open(main_inbox_url, '_self');
+          } else {
+              next_button.click();
+          }
 
         }
-    }
-    if (index > max_index) {
-
-      console.log(`No more unread tags on current page`);
-      var next_button = document.getElementById('Page_Next_1');
-      if (next_button.disabled) {
-          console.log(`This is the last page of the inbox`);
-          window.open(main_inbox_url, '_self');
-      } else {
-          next_button.click();
-      }
-
     }
 }
